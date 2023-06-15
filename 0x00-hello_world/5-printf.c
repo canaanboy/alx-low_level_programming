a84858059570b583248eb3cc396fd0b3951cd6e7
canaanboy <masumbet@yahoo.com>
2023-06-15 10:50:48 -0700

    1st commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-*main - starting point
-*
-*Return: 0
-*/
-int main(void){
-		
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+ * main - Starting point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
