ce9daed65bbcca2c42401ee676db94079a168e7b
canaanboy <masumbet@yahoo.com>
2023-06-15 10:42:41 -0700

    2nd commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-*main - starting point
-*
-*Return: 0
-*/
-int main(void){
-		
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+ * main - Starting point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
