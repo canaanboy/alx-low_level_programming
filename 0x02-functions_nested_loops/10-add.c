d0412e919066d95c71923e17655350e7c83f4def
canaanboy <masumbet@yahoo.com>
2023-06-20 04:35:50 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int z;
+
+	z = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
